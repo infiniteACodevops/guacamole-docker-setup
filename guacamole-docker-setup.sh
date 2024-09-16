d3fee817d0425f701f879eaeaf3e769c916c2d03
infiniteACodevops <64240574+infiniteACodevops@users.noreply.github.com>
2024-09-16 00:14:17 -0300

    Update guacamole-docker-setup.sh
    ajuste com nginx funcionando

--- guacamole-docker-setup.sh
+++ guacamole-docker-setup.sh
@@ -2,45 +2,47 @@
 
 set -e
 
-echo "Iniciando a configuração do ambiente Guacamole..."
+# Diretório onde o repositório será clonado
+REPO_DIR=~/guacamole-docker-setup
 
-# Clone do repositório, caso não esteja presente
-if [ ! -d "guacamole-docker-setup" ]; then
+# Verificar se o repositório já existe
+if [ ! -d "$REPO_DIR" ]; then
   echo "Clonando o repositório..."
-  git clone https://github.com/infiniteACodevops/guacamole-docker-setup.git
+  git clone https://github.com/infiniteACodevops/guacamole-docker-setup.git "$REPO_DIR"
 fi
 
-cd guacamole-docker-setup
+echo "Entrando no diretório do projeto..."
+cd "$REPO_DIR"
 
-# Gerar script de inicialização do banco de dados
-echo "Gerando script de inicialização do banco de dados..."
+echo "Criando diretório init-scripts..."
 mkdir -p init-scripts
+
+echo "Gerando o script de inicialização do banco de dados..."
 docker run --rm guacamole/guacamole:1.5.5 /opt/guacamole/bin/initdb.sh --mysql > init-scripts/initdb.sql
 
-# Ajustar permissões dos arquivos
-echo "Ajustando permissões dos arquivos..."
+echo "Ajustando permissões..."
 sudo chmod 755 init-scripts
 sudo chmod 644 init-scripts/initdb.sql
 
-# Criar configuração do Nginx
-echo "Criando configuração do Nginx..."
-mkdir -p nginx
-cat <<EOT > nginx/nginx.conf
+echo "Criando a configuração do Nginx..."
+cat <<EOT > ./nginx.conf
 server {
     listen 80;
-    server_name 192.168.51.125;
 
     location / {
+        set_real_ip_from 0.0.0.0/0;
+        real_ip_header CF-Connecting-IP;
+        real_ip_recursive on;
+
         proxy_pass http://guacamole:8080/guacamole/;
-        proxy_buffering off;
         proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header Host \$http_host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Forwarded-Hostname \$http_host;
     }
 }
 EOT
 
-# Subir serviços
-echo "Iniciando serviços..."
+echo "Subindo os serviços..."
 docker-compose up -d
-
-echo "Configuração completa! Acesse o Guacamole em http://192.168.51.125/"
